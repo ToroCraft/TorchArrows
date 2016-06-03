9402f3dacd5b5e17a2551b40227dd835bcf7896a
Zane Riley <zeriley@gmail.com>
2016-06-02 22:36:32 -0400

    version step to 1.9.4-2

--- src/main/java/net/torocraft/torcharrowsmod/TorchArrowsMod.java
+++ src/main/java/net/torocraft/torcharrowsmod/TorchArrowsMod.java
@@ -14,7 +14,7 @@ import net.torocraft.torcharrowsmod.entity.projectile.EntityTorchArrow;
 public class TorchArrowsMod {
 	
 	public static final String MODID = "torcharrowsmod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.9.4-2";
 	public static final String MODNAME = "TorchArrowsMod";
 	
 	@SidedProxy(clientSide="net.torocraft.torcharrowsmod.ClientProxy", serverSide="net.torocraft.torcharrowsmod.ServerProxy")
